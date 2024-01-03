a5f0b19c23088ac5d26a108cf12006cf9610ef2f
Alex Boylan <aboylan@users.noreply.github.com>
2024-01-02 20:26:32 -0600

    first commit

---
+++ src/main/java/com/aboylan/springboot/app/usuarios/commons/SpringbootServicioUsuariosCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.aboylan.springboot.app.usuarios.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioUsuariosCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioUsuariosCommonsApplication.class, args);
+	}
+
+}
